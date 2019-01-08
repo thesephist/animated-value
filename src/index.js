6352a107ad525a259e92f242bf6dd9d51251699c
Linus Lee <linus@thesephist.com>
2019-01-08 09:17:22 -0500

    Add code annotations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+//> Animation curves are usually defined as cubic Bezier
+//  curves, but it turns out computing these functions on the fly
+//  from the time domain is pretty tricky. We depend on this ~500B
+//  library to resolve Bezier curves for us.
 import * as Bezier from 'bezier-easing';
 
+//> Linear easing curve
 const identity = t => t;
 
+//> Animation involves lots of measuring time.
+//  This is a shortcut to get the current unix epoch time
+//  in milliseconds.
 const now = () => new Date().getTime();
 
+//> AnimatedValue's animation objects are state machines, with
+//  three states. These three states are represented as these
+//  constants.
 const STATE_UNSTARTED = 0;
 const STATE_PLAYING = 1;
 const STATE_PAUSED = 2;
 
+//> By default, `AnimatedValue.CURVES` provides a useful set of
+//  easing curves we can use out of the box.
 const CURVES = {
     LINEAR: identity,
     EASE: Bezier(0.25, 0.1, 0.25, 1),
@@ -21,15 +34,31 @@ const CURVES = {
     EXPO_IN_OUT: Bezier(1, 0, 0, 1),
 }
 
+//> The `Playable` class represents something whose timeline
+//  can be played, paused, and reset. Both a single AnimatedValue,
+//  as well as `CompositeAnimatedValue` (a combination of more than
+//  one AV) inherit from `Playable`. This class enables us to have
+//  polymorphic, imperative animation control APIs.
 class Playable {
 
     constructor() {
+        //> A `Playable` is a state machine.
         this.state = STATE_UNSTARTED;
+        //> The duration requested for an animation play-through
         this._duration = null;
+        //> When did our current animation run start? `null` if the
+        //  animation is not currently playing.
         this._startTime = null;
+        //> If we are paused, we keep track of how far through the
+        //  animation we were here.
         this._pausedTime = null;
+        //> Callback after each frame is rendered during play
         this._callback = null;
+        //> A promise that resolves when the currently playing animation
+        //  either finishes (resolves to `true`) or is reset (resolves to `false`).
         this._promise = Promise.resolve();
+        //> Temporary variable we use as a pointer to the resolver of `this._promise`,
+        //  so we can resolve the promise outside of the promise body.
         this._promiseResolver = null;
     }
 
@@ -48,6 +77,10 @@ class Playable {
                 if (callback !== undefined) {
                     callback();
                 }
+                //> Sometimes, in between frames, the user
+                //  will pause the animation but we assume it isn't paused
+                //  so we keep playing. This checks if we're supposed to
+                //  still be playing the next frame.
                 if (this.state === STATE_PLAYING) {
                     if (now() - this._startTime > this._duration) {
                         this.pause();
@@ -96,6 +129,10 @@ class Playable {
 
 }
 
+//> `AnimatedValue` represents a single value (number) that can
+//  be animated with a duration and an easing curve. Most often,
+//  an `AnimatedValue` corresponds to a single CSS property that
+//  we're animating on a component, like translate / scale / opacity.
 class AnimatedValue extends Playable {
 
     constructor({
@@ -106,20 +143,33 @@ class AnimatedValue extends Playable {
         super();
         this.start = start;
         this.end = end;
+        //> We accept an array of cubic Bezier points as an easing function,
+        //  in which case we create a Bezier curve out of them.
         this.ease = Array.isArray(ease) ? Bezier(...ease) : ease;
+        //> The fill state represents the value of the animated value whenever
+        //  the animation is not running. The value is initialized to the start position.
         this._fillState = start;
     }
 
+    //> Statically defined so consumers of the API can define easing curves
+    //  as `AnimatedValue.CURVES.[CURVE_NAME]`.
     static get CURVES() {
         return CURVES;
     }
 
-    static compose(...animatedValues) {
-        return new CompositeAnimatedValue(animatedValues);
+    //> Statically defined constructor for a composite animation, which takes
+    //  multiple `Playable`s (either single or composite animated values) and
+    //  plays all of them concurrently.
+    static compose(...playables) {
+        return new CompositeAnimatedValue(playables);
     }
 
+    //> What's the current numerical value of this animated value?
+    //  This API is intentionally not implemented as a getter, to communicate
+    //  to the API consumer that value computation has a nonzero cost with each access.
     value() {
-        if (this.state !== STATE_PLAYING) { // means it's paused or finished
+        if (this.state !== STATE_PLAYING) {
+            //> If the animation is not playing, just return the last fill value
             return this._fillState;
         } else {
             const elapsedTime = now() - this._startTime;
@@ -130,7 +180,7 @@ class AnimatedValue extends Playable {
 
     pause() {
         if (this.state === STATE_PLAYING) {
-            //> The order matters here, because we can't get the value if we aren't playing
+            //> The order of `super` call matters here, because we can't get the value if we aren't playing
             this._fillState = this.value();
         }
         super.pause();
@@ -143,18 +193,22 @@ class AnimatedValue extends Playable {
 
 }
 
+//> A `CompositeAnimatedValue` is a `Playable` wrapper around many (single, composite) animated values,
+//  that can run all of the animations in the same duration, concurrently. `CompositeAnimatedValue`
+//  is polymorphic, and can take any `Playable` as a sub-animation.
 class CompositeAnimatedValue extends Playable {
 
-    constructor(animatedValues) {
+    constructor(playables) {
         super();
-        this._animatedValues = animatedValues;
+        this._playables = playables;
     }
 
     play(duration, callback) {
-        //> Play swallows the callback here and calls it once
-        //  for the entire composition, efficiently.
+        //> `CompositeAnimatedValue#play()` swallows the callback here and calls it once
+        //  for the entire composition, efficiently, so the callback isn't called N
+        //  times for N animated values below this composite animation.
         const ret = super.play(duration, callback);
-        for (const av of this._animatedValues) {
+        for (const av of this._playables) {
             av.play(duration);
         }
         return ret;
@@ -162,24 +216,24 @@ class CompositeAnimatedValue extends Playable {
 
     pause() {
         super.pause();
-        for (const av of this._animatedValues) {
+        for (const av of this._playables) {
             av.pause();
         }
     }
 
     resume() {
         if (this.state === STATE_PAUSED) {
-            for (const av of this._animatedValues) {
+            for (const av of this._playables) {
                 av.resume();
             }
         }
-        //> Order is important -- if we resume first, the checks above don't work
+        //> Order of `super.resume()` call is important -- if we resume first, the checks above don't work
         super.resume();
     }
 
     reset() {
         super.reset();
-        for (const av of this._animatedValues) {
+        for (const av of this._playables) {
             av.reset();
         }
     }
